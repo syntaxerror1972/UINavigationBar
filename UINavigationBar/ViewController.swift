909c5c1954b47be290480afa280a7908854b397c
Shrawan Shinde <shrawanshinde@Shrawans-MacBook-Pro.local>
2017-01-19 00:39:36 +0530

    A UINavigationBar object is a bar, typically displayed at the top of the window, containing buttons for navigating within a hierarchy of screens. The primary components are a left (back) button, a center title, and an optional right button. You can use a navigation bar as a standalone object or in conjunction with a navigation controller object.

--- UINavigationBar/ViewController.swift
+++ UINavigationBar/ViewController.swift
@@ -13,6 +13,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor=UIColor.white
+        let navBar: UINavigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: getWindowWidth(), height: 60))
+        self.view.addSubview(navBar);
+        let navItem = UINavigationItem(title: "Navigation Bar");
+        let doneItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: nil, action: "doneClick");
+        navItem.rightBarButtonItem = doneItem;
+        
+        
+        navBar.setItems([navItem], animated: false);
+        
+        self.view.addSubview(navBar)
+    }
+    
+    func doneClick(){
+        
+        //Write button action here
+        var alertView = UIAlertView();
+        alertView.addButton(withTitle: "Ok");
+        alertView.title = "Alert";
+        alertView.message = "Done Button Clicked";
+        alertView.show();
+        
     }
 
     override func didReceiveMemoryWarning() {
